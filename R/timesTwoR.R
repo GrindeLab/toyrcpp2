9d2ca515c623b7682dca02b6a01340d9f9e74cf0
Kelsey Grinde <kgrinde@macalester.edu>
2020-07-08 16:46:12 -0500

    create timesTwoR function with roxygen comments

---
+++ R/timesTwoR.R
@@ -0,0 +1,16 @@
+#' Multiply by two (R version).
+#'
+#' This function multiplies numbers by two.
+#'
+#' @param x Numeric vector
+#'
+#' @return A numeric vector that is double the input vector
+#'
+#' @examples
+#' timesTwoR(2)
+#' timesTwoR(1:3)
+#'
+#' @export
+timesTwoR <- function(x) {
+  x * 2
+}
